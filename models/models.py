72279ac4ddb7c566feede933a7c22d30b539e6de
Haroon <haroon.hassan@gmail.com>
2018-04-21 16:56:10 +0100

    modified some stuff

--- models/models.py
+++ models/models.py
@@ -1,19 +1,20 @@
 import pandas as pd
 
 
+class Segment:
+
+    def __init__(self, name, stocks_df):
+        self._name = name
+        self._stocks = stocks_df
+
+
 class Market:
 
     def __init__(self, name, start, end, df):
         self._name = name
         self._start = start
         self._end = end
-
-    
-
-class Segment:
-
-    def __init(self, name):
-        pass
+        self._segments = {segment[0]: Segment(segment[0], segment[1]) for segment in df.groupby('segment')}
 
 
 class Stock:
