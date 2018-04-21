c932e7595dd3b9c81b031cefb830a91bee4cfb64
Haroon <haroon.hassan@gmail.com>
2018-04-21 15:19:02 +0100

    created more models

--- models/models.py
+++ models/models.py
@@ -3,5 +3,20 @@ import pandas as pd
 
 class Market:
 
-    def __init__(self, name, start, end, stock_path):
+    def __init__(self, name, start, end, df):
+        self._name = name
+        self._start = start
+        self._end = end
+
+    
+
+class Segment:
+
+    def __init(self, name):
+        pass
+
+
+class Stock:
+
+    def __init__(self, id, name, initial, segment):
         pass
