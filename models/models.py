509bfdf3b3a57cf0c7aa4ff91748d5c23dafa275
Shun Liang <shun.liang.public@gmail.com>
2018-04-21 17:55:59 +0100

    Implement Stock constructor

--- models/models.py
+++ models/models.py
@@ -4,20 +4,42 @@ import pandas as pd
 class Segment:
 
     def __init__(self, name, stocks_df):
-        self._name = name
-        self._stocks = stocks_df
+        self.name = name
+        stocks_df['models'] = stocks_df.apply(
+            lambda x: Stock(
+                stock_id=x['id'],
+                name=x['name'],
+                initial_price=x['initial_price'],
+                initial_market_cap=x['initial_market_cap'],
+                distribution=x['distribution'],
+                volatility=x['volatility'],
+                mean=x['mean'],
+            ),
+            axis=1,
+        )
+        df = stocks_df[['id', 'models']]
+        df.index = df['id']
+        df = df.drop('id', axis=1)
+        stocks_dict = df.to_dict()['models']
+        self.stocks = stocks_dict
 
 
 class Market:
 
     def __init__(self, name, start, end, df):
-        self._name = name
-        self._start = start
-        self._end = end
-        self._segments = {segment[0]: Segment(segment[0], segment[1]) for segment in df.groupby('segment')}
+        self.name = name
+        self.start = start
+        self.end = end
+        self.segments = {segment[0]: Segment(segment[0], segment[1]) for segment in df.groupby('segment')}
 
 
 class Stock:
 
-    def __init__(self, id, name, initial, segment):
-        pass
+    def __init__(self, stock_id, name, initial_price, initial_market_cap, distribution, volatility, mean):
+        self.stock_id = stock_id
+        self.name = name
+        self.initial_price = initial_price
+        self.initial_market_cap = initial_market_cap
+        self.distribution = distribution
+        self.volatility = volatility
+        self.mean = mean
