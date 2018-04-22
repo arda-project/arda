b7406d660a771866f0371be2f5fa8172ca2f071e
Erik Wellner <ewellner@gmail.com>
2018-04-22 10:56:28 +0200

    integrating new algo for generating returns for stocks

--- loader/input.py
+++ loader/input.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from models.models import Market
 
+import datetime
 
 def load_data(name, start, end, path):
     try:
@@ -13,3 +14,10 @@ def load_data(name, start, end, path):
 
 def market_builder(name, start, end, df):
     return Market(name, start, end, df)
+
+if __name__ == "__main__":
+
+    # create market object
+    market = load_data('Test',datetime.date(2018,1,1), datetime.date(2019,1,1), r'C:\Users\Erik\ardaProject\arda\stocks.csv')
+
+    report = market.simulate(0.5,0.5)
